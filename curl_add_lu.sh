d87f9affe05b102f3e8dbff6dfe09e4e4b5a54dd
Alain Dejoux <adejoux@krystalia.net>
2014-09-11 11:39:29 +0200

    small fix on curl_add_lu.sh

--- curl_add_lu.sh
+++ curl_add_lu.sh
@@ -2,4 +2,4 @@ HMC=${HMC:-myhmc}
 CLUSTERUUID=${CLUSTERUUID:-95d38275-ab43-333e-a472-bb4aab38645b}
 
 [ ! -f cookies.txt ] && echo "Please login first with curl_login.sh" && exit 1
-curl -k -c cookies.txt -b cookies.txt -H "Accept: application/atom+xml; charset=UTF-8" https://${HMC}:12443/rest/api/uom/Cluster/${CLUSTERUUID}/do/CreateLogicalUnit
+curl -k -c cookies.txt -b cookies.txt -i -H "Content-Type: application/vnd.ibm.powervm.web+xml; type=JobRequest" -H "Accept: application/atom+xml; charset=UTF-8" -H "X-Audit-Memento: addlu" -X PUT -H "Expect:" -d @add_lu.xml https://${HMC}:12443/rest/api/uom/Cluster/${CLUSTERUUID}/do/CreateLogicalUnit
